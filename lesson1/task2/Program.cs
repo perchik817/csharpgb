9a1a132cbf216ed377798a636e624936410edad3
Perchik <saikalrustamovna@gmail.com>
2024-01-23 17:27:24 +0600

    while

---
+++ lesson1/task2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10, i = 1;
+            while (i <= n)
+            {
+                Console.Write(i + ", ");
+                i++;
+            }
+        }
+    }
+}
